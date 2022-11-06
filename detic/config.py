c890de1b5290d85eb7e01035f0b962505b7dba24
empMEMORY <165358906+empMEMORY@users.noreply.github.com>
2022-11-06 18:45:08 +0000

    Update Chinese (Taiwan) translation

--- detic/config.py
+++ detic/config.py
@@ -7,4 +7,16 @@ def add_detic_config(cfg):
     _C.WITH_IMAGE_LABELS = False # Turn on co-training with classification data
 
     # Open-vocabulary classifier
-    _C.MODEL.ROI_BOX_HEAD.USE_ZEROSHOT_CLS = False # Use fixed classifier for open-vocabula
+    _C.MODEL.ROI_BOX_HEAD.USE_ZEROSHOT_CLS = False # Use fixed classifier for open-vocabulary detection
+    _C.MODEL.ROI_BOX_HEAD.ZEROSHOT_WEIGHT_PATH = 'datasets/metadata/lvis_v1_clip_a+cname.npy'
+    _C.MODEL.ROI_BOX_HEAD.ZEROSHOT_WEIGHT_DIM = 512
+    _C.MODEL.ROI_BOX_HEAD.NORM_WEIGHT = True
+    _C.MODEL.ROI_BOX_HEAD.NORM_TEMP = 50.0
+    _C.MODEL.ROI_BOX_HEAD.IGNORE_ZERO_CATS = False
+    _C.MODEL.ROI_BOX_HEAD.USE_BIAS = 0.0 # >= 0: not use
+    
+    _C.MODEL.ROI_BOX_HEAD.MULT_PROPOSAL_SCORE = False # CenterNet2
+    _C.MODEL.ROI_BOX_HEAD.USE_SIGMOID_CE = False
+    _C.MODEL.ROI_BOX_HEAD.PRIOR_PROB = 0.01
+    _C.MODEL.ROI_BOX_HEAD.USE_FED_LOSS = False # Federated Loss
+    _C.MODEL.ROI_BOX_HEAD.CAT_FREQ_PATH 
