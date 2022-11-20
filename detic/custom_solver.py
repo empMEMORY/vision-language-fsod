8f45a799866c7aec319f0a36e8b500f96b16121d
empMEMORY <165358906+empMEMORY@users.noreply.github.com>
2022-11-20 20:47:42 +0000

    fixed small bug

--- detic/custom_solver.py
+++ detic/custom_solver.py
@@ -16,4 +16,20 @@ def match_name_keywords(n, name_keywords):
             break
     return out
 
-def build_custo
+def build_custom_optimizer(cfg: CfgNode, model: torch.nn.Module) -> torch.optim.Optimizer:
+    """
+    Build an optimizer from config.
+    """
+    params: List[Dict[str, Any]] = []
+    memo: Set[torch.nn.parameter.Parameter] = set()
+    custom_multiplier_name = cfg.SOLVER.CUSTOM_MULTIPLIER_NAME
+    optimizer_type = cfg.SOLVER.OPTIMIZER
+    for key, value in model.named_parameters(recurse=True):
+        if not value.requires_grad:
+            continue
+        # Avoid duplicating parameters
+        if value in memo:
+            continue
+        if cfg.SOLVER.FREEZE_BACKBONE:
+            if 'backbone' in key:
+    
