417daecfd96732aa2a049483a74c4158ae950a99
empMEMORY <165358906+empMEMORY@users.noreply.github.com>
2022-12-02 18:03:34 +0000

    Resolve merge conflict.

--- detic/data/custom_build_augmentation.py
+++ detic/data/custom_build_augmentation.py
@@ -1,1 +1,22 @@
-# Copyright (c) 
+# Copyright (c) Facebook, Inc. and its affiliates.
+import logging
+import numpy as np
+import pycocotools.mask as mask_util
+import torch
+from fvcore.common.file_io import PathManager
+from PIL import Image
+
+
+from detectron2.data import transforms as T
+from .transforms.custom_augmentation_impl import EfficientDetResizeCrop
+
+def build_custom_augmentation(cfg, is_train, scale=None, size=None, \
+    min_size=None, max_size=None):
+    """
+    Create a list of default :class:`Augmentation` from config.
+    Now it includes resizing and flipping.
+
+    Returns:
+        list[Augmentation]
+    """
+    if cfg.INPUT.CUSTOM_AUG == 'Re
