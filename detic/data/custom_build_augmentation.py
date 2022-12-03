fd389b98b0d1faf544bd045b9d7764d1e2e771f5
empMEMORY <165358906+empMEMORY@users.noreply.github.com>
2022-12-03 19:56:47 +0000

    Update docstrings

--- detic/data/custom_build_augmentation.py
+++ detic/data/custom_build_augmentation.py
@@ -19,4 +19,16 @@ def build_custom_augmentation(cfg, is_train, scale=None, size=None, \
     Returns:
         list[Augmentation]
     """
-    if cfg.INPUT.CUSTOM_AUG == 'Re
+    if cfg.INPUT.CUSTOM_AUG == 'ResizeShortestEdge':
+        if is_train:
+            min_size = cfg.INPUT.MIN_SIZE_TRAIN if min_size is None else min_size
+            max_size = cfg.INPUT.MAX_SIZE_TRAIN if max_size is None else max_size
+            sample_style = cfg.INPUT.MIN_SIZE_TRAIN_SAMPLING
+        else:
+            min_size = cfg.INPUT.MIN_SIZE_TEST
+            max_size = cfg.INPUT.MAX_SIZE_TEST
+            sample_style = "choice"
+        augmentation = [T.ResizeShortestEdge(min_size, max_size, sample_style)]
+    elif cfg.INPUT.CUSTOM_AUG == 'EfficientDetResizeCrop':
+        if is_train:
+            scale = cfg.INPUT.SCALE_RANGE 
