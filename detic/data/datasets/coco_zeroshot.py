b7cddd1de6861d8439a84c4ada9522e90e955232
empMEMORY <165358906+empMEMORY@users.noreply.github.com>
2023-01-14 05:26:16 +0000

    fix untracked files

--- detic/data/datasets/coco_zeroshot.py
+++ detic/data/datasets/coco_zeroshot.py
@@ -1,1 +1,19 @@
-# Copyright (c) Facebook, Inc. and its affil
+# Copyright (c) Facebook, Inc. and its affiliates.
+import os
+
+from detectron2.data.datasets.register_coco import register_coco_instances
+from detectron2.data.datasets.builtin_meta import _get_builtin_metadata
+from .lvis_v1 import custom_register_lvis_instances
+
+categories_seen = [
+    {'id': 1, 'name': 'person'},
+    {'id': 2, 'name': 'bicycle'},
+    {'id': 3, 'name': 'car'},
+    {'id': 4, 'name': 'motorcycle'},
+    {'id': 7, 'name': 'train'},
+    {'id': 8, 'name': 'truck'},
+    {'id': 9, 'name': 'boat'},
+    {'id': 15, 'name': 'bench'},
+    {'id': 16, 'name': 'bird'},
+    {'id': 19, 'name': 'horse'},
+    {'id
