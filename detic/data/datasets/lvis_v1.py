836d1637b4df1aaa7b64db00de87d2fb1f4fa224
empMEMORY <165358906+empMEMORY@users.noreply.github.com>
2023-02-01 07:02:32 +0000

    Updated read me file

--- detic/data/datasets/lvis_v1.py
+++ detic/data/datasets/lvis_v1.py
@@ -6,4 +6,25 @@ from fvcore.common.timer import Timer
 from detectron2.structures import BoxMode
 from fvcore.common.file_io import PathManager
 from detectron2.data import DatasetCatalog, MetadataCatalog
-from detectron2.data.datasets
+from detectron2.data.datasets.lvis import get_lvis_instances_meta
+
+logger = logging.getLogger(__name__)
+
+__all__ = ["custom_load_lvis_json", "custom_register_lvis_instances"]
+
+
+def custom_register_lvis_instances(name, metadata, json_file, image_root):
+    """
+    """
+    DatasetCatalog.register(name, lambda: custom_load_lvis_json(
+        json_file, image_root, name))
+    MetadataCatalog.get(name).set(
+        json_file=json_file, image_root=image_root, 
+        evaluator_type="lvis", **metadata
+    )
+
+
+def custom_load_lvis_json(json_file, image_root, dataset_name=None):
+    '''
+    Modifications:
+      use `f
