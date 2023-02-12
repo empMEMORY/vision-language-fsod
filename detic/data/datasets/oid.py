88dc333eec6b5c8528e23ddafc436a0eba8fcec4
empMEMORY <165358906+empMEMORY@users.noreply.github.com>
2023-02-12 18:49:36 +0000

    Add SVG equation

---
+++ detic/data/datasets/oid.py
@@ -0,0 +1,3 @@
+# Part of the code is from https://github.com/xingyizhou/UniDet/blob/master/projects/UniDet/unidet/data/datasets/oid.py
+# Copyright (c) Facebook, Inc. and its affiliates.
+from .register_oid import register_oid_i
