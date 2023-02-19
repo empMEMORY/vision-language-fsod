e9e01cd063205e323a4a87a20ed5a7eadc37db1d
empMEMORY <165358906+empMEMORY@users.noreply.github.com>
2023-02-19 09:57:23 +0000

    Some more

--- detic/data/datasets/oid.py
+++ detic/data/datasets/oid.py
@@ -13,4 +13,13 @@ categories = [
     {'id': 7, 'name': 'Animal', 'freebase_id': '/m/0jbk'},
     {'id': 8, 'name': 'Glove', 'freebase_id': '/m/0174n1'},
     {'id': 9, 'name': 'Crocodile', 'freebase_id': '/m/09f_2'},
-    {'id': 10, 'name': 'Cattle', 'freebase_
+    {'id': 10, 'name': 'Cattle', 'freebase_id': '/m/01xq0k1'},
+    {'id': 11, 'name': 'House', 'freebase_id': '/m/03jm5'},
+    {'id': 12, 'name': 'Guacamole', 'freebase_id': '/m/02g30s'},
+    {'id': 13, 'name': 'Penguin', 'freebase_id': '/m/05z6w'},
+    {'id': 14, 'name': 'Vehicle registration plate', 'freebase_id': '/m/01jfm_'},
+    {'id': 15, 'name': 'Bench', 'freebase_id': '/m/076lb9'},
+    {'id': 16, 'name': 'Ladybug', 'freebase_id': '/m/0gj37'},
+    {'id': 17, 'name': 'Human nose', 'freebase_id': '/m/0k0pj'},
+    {'id': 18, 'name': 'Watermelon', 'freebase_id': '/m/0kpqd'},
+    {'id': 19, 'name': 'Flute', 'fre
