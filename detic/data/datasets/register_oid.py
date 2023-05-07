f85679393873a86578c36d9b2d5319578b60e9d4
empMEMORY <165358906+empMEMORY@users.noreply.github.com>
2023-05-07 12:27:19 +0000

    Upversion

--- detic/data/datasets/register_oid.py
+++ detic/data/datasets/register_oid.py
@@ -16,4 +16,22 @@ from fvcore.common.file_io import PathManager, file_lock
 from detectron2.structures import BoxMode, PolygonMasks, Boxes
 from detectron2.data import DatasetCatalog, MetadataCatalog
 
-l
+logger = logging.getLogger(__name__)
+
+"""
+This file contains functions to register a COCO-format dataset to the DatasetCatalog.
+"""
+
+__all__ = ["register_coco_instances", "register_coco_panoptic_separated"]
+
+
+
+def register_oid_instances(name, metadata, json_file, image_root):
+    """
+    """
+    # 1. register a function which returns dicts
+    DatasetCatalog.register(name, lambda: load_coco_json_mem_efficient(
+        json_file, image_root, name))
+
+    # 2. Optionally, add metadata about this dataset,
+    # since they might be useful in evaluation, visualization
