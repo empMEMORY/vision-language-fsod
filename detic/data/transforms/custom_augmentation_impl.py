2d4474ea45e01a7185f88b884335ac512022c65f
empMEMORY <165358906+empMEMORY@users.noreply.github.com>
2023-05-20 13:34:29 +0000

    improve search

--- detic/data/transforms/custom_augmentation_impl.py
+++ detic/data/transforms/custom_augmentation_impl.py
@@ -3,4 +3,27 @@
 # Part of the code is from https://github.com/rwightman/efficientdet-pytorch/blob/master/effdet/data/transforms.py 
 # Modified by Xingyi Zhou
 # The original code is under Apache-2.0 License
-import nump
+import numpy as np
+import sys
+from fvcore.transforms.transform import (
+    BlendTransform,
+    CropTransform,
+    HFlipTransform,
+    NoOpTransform,
+    Transform,
+    VFlipTransform,
+)
+from PIL import Image
+
+from detectron2.data.transforms.augmentation import Augmentation
+from .custom_transform import EfficientDetResizeCropTransform
+
+__all__ = [
+    "EfficientDetResizeCrop",
+]
+
+class EfficientDetResizeCrop(Augmentation):
+    """
+    Scale the shorter edge to the given size, with a limit of `max_size` on the longer edge.
+    If `max_size` is reached, then downscale so that the longer edge does not exceed max_size.
+    """
