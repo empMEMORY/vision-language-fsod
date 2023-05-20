faae1c44bbc69021cf4e24e6963a0fe380260765
empMEMORY <165358906+empMEMORY@users.noreply.github.com>
2023-05-20 14:10:33 +0000

    fix styling issues

--- detic/data/transforms/custom_augmentation_impl.py
+++ detic/data/transforms/custom_augmentation_impl.py
@@ -27,3 +27,21 @@ class EfficientDetResizeCrop(Augmentation):
     Scale the shorter edge to the given size, with a limit of `max_size` on the longer edge.
     If `max_size` is reached, then downscale so that the longer edge does not exceed max_size.
     """
+
+    def __init__(
+        self, size, scale, interp=Image.BILINEAR
+    ):
+        """
+        """
+        super().__init__()
+        self.target_size = (size, size)
+        self.scale = scale
+        self.interp = interp
+
+    def get_transform(self, img):
+        # Select a random scale factor.
+        scale_factor = np.random.uniform(*self.scale)
+        scaled_target_height = scale_factor * self.target_size[0]
+        scaled_target_width = scale_factor * self.target_size[1]
+        # Recompute the accurate scale_factor using rounded scaled image size.
+        width, height = img.shape[1], im
