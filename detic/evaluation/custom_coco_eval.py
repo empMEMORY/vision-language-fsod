a90ea31e390e834d9a9936531c7bb82062354878
empMEMORY <165358906+empMEMORY@users.noreply.github.com>
2023-05-25 22:14:33 +0000

    v5.1.1

--- detic/evaluation/custom_coco_eval.py
+++ detic/evaluation/custom_coco_eval.py
@@ -13,4 +13,17 @@ import pycocotools.mask as mask_util
 import torch
 from pycocotools.coco import COCO
 from pycocotools.cocoeval import COCOeval
-from tabulate import tabul
+from tabulate import tabulate
+
+import detectron2.utils.comm as comm
+from detectron2.config import CfgNode
+from detectron2.data import MetadataCatalog
+from detectron2.data.datasets.coco import convert_to_coco_json
+from detectron2.evaluation.coco_evaluation import COCOEvaluator
+from detectron2.structures import Boxes, BoxMode, pairwise_iou
+from detectron2.utils.file_io import PathManager
+from detectron2.utils.logger import create_small_table
+from ..data.datasets.coco_zeroshot import categories_seen, categories_unseen
+
+class CustomCOCOEvaluator(COCOEvaluator):
+    def _derive_coco_results(self, coco_
