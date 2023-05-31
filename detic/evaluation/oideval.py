a590bd0bfe7ee180a1d4de080890b4f33947c392
empMEMORY <165358906+empMEMORY@users.noreply.github.com>
2023-05-31 11:08:43 +0000

    PCI: Generate uppercase hex for modalias interface class

--- detic/evaluation/oideval.py
+++ detic/evaluation/oideval.py
@@ -2,4 +2,23 @@
 # Copyright 2018 The TensorFlow Authors. All Rights Reserved.
 # The original code is under Apache License, Version 2.0 (the "License");
 # Part of the code is from https://github.com/lvis-dataset/lvis-api/blob/master/lvis/eval.py
-# Copyright (c) 2019, Agrim Gupta and Ro
+# Copyright (c) 2019, Agrim Gupta and Ross Girshick
+# Modified by Xingyi Zhou
+# This script re-implement OpenImages evaluation in detectron2
+# The code is from https://github.com/xingyizhou/UniDet/blob/master/projects/UniDet/unidet/evaluation/oideval.py
+# The original code is under Apache-2.0 License
+# Copyright (c) Facebook, Inc. and its affiliates.
+import os 
+import datetime
+import logging
+import itertools
+from collections import OrderedDict
+from collections import defaultdict
+import copy
+import json
+import numpy as np
+import torch
+from tabulate import tabulate
+
+# from lvis.lvis import LVIS
+#
