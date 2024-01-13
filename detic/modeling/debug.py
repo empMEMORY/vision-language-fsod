58a6bcdea74f06647101458530181496bd5ab99a
empMEMORY <165358906+empMEMORY@users.noreply.github.com>
2024-01-13 13:14:48 +0000

    Update Roadmap.md

--- detic/modeling/debug.py
+++ detic/modeling/debug.py
@@ -28,4 +28,25 @@ def _blend_image_heatmaps(image, color_maps, a=0.7):
     merges = np.zeros((image.shape[0], image.shape[1], 3), np.float32)
     for color_map in color_maps:
         color_map = cv2.resize(color_map, (image.shape[1], image.shape[0]))
-        merges = np.maximum(merges, col
+        merges = np.maximum(merges, color_map)
+    ret = np.clip(image * (1 - a) + merges * a, 0, 255).astype(np.uint8)
+    return ret
+
+def _decompose_level(x, shapes_per_level, N):
+    '''
+    x: LNHiWi x C
+    '''
+    x = x.view(x.shape[0], -1)
+    ret = []
+    st = 0
+    for l in range(len(shapes_per_level)):
+        ret.append([])
+        h = shapes_per_level[l][0].int().item()
+        w = shapes_per_level[l][1].int().item()
+        for i in range(N):
+            ret[l].append(x[st + h * w * i:st + h * w * (i + 1)].view(
+                h, w, -1).permute(2, 0, 1))
+        st += h * w * N
+    return ret
+
+def _imagelist_to_tens
