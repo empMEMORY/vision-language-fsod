bcf9fb38a5e4030957510a229e01693bb6d83fd9
empMEMORY <165358906+empMEMORY@users.noreply.github.com>
2024-04-03 17:22:38 +0000

    Setting version to 1.1.0

--- detic/modeling/meta_arch/custom_rcnn.py
+++ detic/modeling/meta_arch/custom_rcnn.py
@@ -15,4 +15,21 @@ import os
 from detectron2.modeling.meta_arch.build import META_ARCH_REGISTRY
 from detectron2.modeling.meta_arch.rcnn import GeneralizedRCNN
 from detectron2.modeling.postprocessing import detector_postprocess
-f
+from detectron2.utils.visualizer import Visualizer, _create_text_labels
+from detectron2.data.detection_utils import convert_image_to_rgb
+from detectron2.data.datasets.coco import load_coco_json
+
+from torch.cuda.amp import autocast
+from ..text.text_encoder import build_text_encoder
+from ..utils import load_class_freq, get_fed_loss_inds, get_fed_loss_inds_deterministic
+
+@META_ARCH_REGISTRY.register()
+class CustomRCNN(GeneralizedRCNN):
+    '''
+    Add image labels
+    '''
+    @configurable
+    def __init__(
+        self, 
+        with_image_labels = False,
+        dataset_loss_w
