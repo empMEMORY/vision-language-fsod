657934f73323f8c64e10d460b5e7f6192a2feeac
empMEMORY <165358906+empMEMORY@users.noreply.github.com>
2024-04-03 14:23:53 +0000

    small code fix

--- detic/modeling/meta_arch/custom_rcnn.py
+++ detic/modeling/meta_arch/custom_rcnn.py
@@ -1,1 +1,18 @@
-# Copyright (c) Facebook, Inc. and its 
+# Copyright (c) Facebook, Inc. and its affiliates.
+import copy
+import logging
+import numpy as np
+from typing import Dict, List, Optional, Tuple
+import torch
+from torch import nn
+import json
+from collections import defaultdict
+from detectron2.utils.events import get_event_storage
+from detectron2.config import configurable
+from detectron2.structures import ImageList, Instances, Boxes
+import detectron2.utils.comm as comm
+import os
+from detectron2.modeling.meta_arch.build import META_ARCH_REGISTRY
+from detectron2.modeling.meta_arch.rcnn import GeneralizedRCNN
+from detectron2.modeling.postprocessing import detector_postprocess
+f
