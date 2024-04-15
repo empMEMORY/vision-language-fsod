5b6097f179e57f1973bfe22f2fb6c700410e5e58
empMEMORY <165358906+empMEMORY@users.noreply.github.com>
2024-04-15 16:04:36 +0000

    krazy fixes

--- detic/predictor.py
+++ detic/predictor.py
@@ -11,4 +11,19 @@ from detectron2.engine.defaults import DefaultPredictor
 from detectron2.utils.video_visualizer import VideoVisualizer
 from detectron2.utils.visualizer import ColorMode, Visualizer
 
-from .modeling.utils im
+from .modeling.utils import reset_cls_test
+
+
+def get_clip_embeddings(vocabulary, prompt='a '):
+    from detic.modeling.text.text_encoder import build_text_encoder
+    text_encoder = build_text_encoder(pretrain=True)
+    text_encoder.eval()
+    texts = [prompt + x for x in vocabulary]
+    emb = text_encoder(texts).detach().permute(1, 0).contiguous().cpu()
+    return emb
+
+BUILDIN_CLASSIFIER = {
+    'lvis': 'datasets/metadata/lvis_v1_clip_a+cname.npy',
+    'objects365': 'datasets/metadata/o365_clip_a+cnamefix.npy',
+    'openimages': 'datasets/metadata/oid_clip_a+cname.npy',
+    'coco': 'datasets/metadata/co
