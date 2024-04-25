78a54ef421ec44059c720cfeb6da705017d0d0c9
empMEMORY <165358906+empMEMORY@users.noreply.github.com>
2024-04-25 19:38:36 +0000

    Fixed a typo in README.md

--- tools/get_lvis_cat_info.py
+++ tools/get_lvis_cat_info.py
@@ -4,4 +4,18 @@ import json
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--ann", default='datasets/lv
+    parser.add_argument("--ann", default='datasets/lvis/lvis_v1_train.json')
+    parser.add_argument("--add_freq", action='store_true')
+    parser.add_argument("--r_thresh", type=int, default=10)
+    parser.add_argument("--c_thresh", type=int, default=100)
+    args = parser.parse_args()
+
+    print('Loading', args.ann)
+    data = json.load(open(args.ann, 'r'))
+    cats = data['categories']
+    image_count = {x['id']: set() for x in cats}
+    ann_count = {x['id']: 0 for x in cats}
+    for x in data['annotations']:
+        image_count[x['category_id']].add(x['image_id'])
+        ann_count[x['category_id']] += 1
+    num_freqs = {x: 0 for x in ['r', 'f', '
