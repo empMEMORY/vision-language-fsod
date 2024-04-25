4bec748769fc789e672028859d0e5ce2ea6539a3
empMEMORY <165358906+empMEMORY@users.noreply.github.com>
2024-04-25 05:30:12 +0000

    django.po: Update German translations

--- tools/get_imagenet_21k_full_tar_json.py
+++ tools/get_imagenet_21k_full_tar_json.py
@@ -17,4 +17,15 @@ sys.path.insert(0, 'third_party/Deformable-DETR')
 from detic.data.tar_dataset import DiskTarDataset, _TarDataset
 
 if __name__ == '__main__':
-    parser = argpar
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--imagenet_dir", default='datasets/imagenet/ImageNet-21k/')
+    parser.add_argument("--tarfile_path", default='datasets/imagenet/metadata-22k/tar_files.npy')
+    parser.add_argument("--tar_index_dir", default='datasets/imagenet/metadata-22k/tarindex_npy')
+    parser.add_argument("--out_path", default='datasets/imagenet/annotations/imagenet-22k_image_info.json')
+    parser.add_argument("--workers", default=16, type=int)
+    args = parser.parse_args()
+
+
+    start_time = time.time()
+    print('Building dataset')
+    dataset = DiskTarDataset(args.tarfile_path, args.tar_i
