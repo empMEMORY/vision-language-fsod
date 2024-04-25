8ffe33ea9e2349ce1f7ad9dfaa3a9c07e021a5ba
empMEMORY <165358906+empMEMORY@users.noreply.github.com>
2024-04-25 20:08:07 +0000

    drbd: fix for deadlock when using automatic split-brain-recovery

--- tools/get_lvis_cat_info.py
+++ tools/get_lvis_cat_info.py
@@ -18,4 +18,21 @@ if __name__ == '__main__':
     for x in data['annotations']:
         image_count[x['category_id']].add(x['image_id'])
         ann_count[x['category_id']] += 1
-    num_freqs = {x: 0 for x in ['r', 'f', '
+    num_freqs = {x: 0 for x in ['r', 'f', 'c']}
+    for x in cats:
+        x['image_count'] = len(image_count[x['id']])
+        x['instance_count'] = ann_count[x['id']]
+        if args.add_freq:
+            freq = 'f'
+            if x['image_count'] < args.c_thresh:
+                freq = 'c'
+            if x['image_count'] < args.r_thresh:
+                freq = 'r'
+            x['frequency'] = freq
+            num_freqs[freq] += 1
+    print(cats)
+    image_counts = sorted([x['image_count'] for x in cats])
+    # ann_counts = sorted([x['instance_count'] for x in cats])
+    # print('image count', image_counts)
+    # import pdb; pdb.set_trace()
+    if args
