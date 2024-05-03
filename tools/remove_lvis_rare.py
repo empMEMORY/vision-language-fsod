ca63782796be6f5cc8d6a6c8e324c94bc8aa59d2
empMEMORY <165358906+empMEMORY@users.noreply.github.com>
2024-05-03 14:46:13 +0000

    Fixed export

--- tools/remove_lvis_rare.py
+++ tools/remove_lvis_rare.py
@@ -12,4 +12,9 @@ if __name__ == '__main__':
     catid2freq = {x['id']: x['frequency'] for x in data['categories']}
     print('ori #anns', len(data['annotations']))
     exclude = ['r']
-    data['annotations'] = [x for x in data['annotati
+    data['annotations'] = [x for x in data['annotations'] \
+        if catid2freq[x['category_id']] not in exclude]
+    print('filtered #anns', len(data['annotations']))
+    out_path = args.ann[:-5] + '_norare.json'
+    print('Saving to', out_path)
+    json.dump(data, open(out_path, 'w'))
